6d2386eff2d0f379a8c66bd54d53fdfae8ea36d5
Malikoto <bigbig2540@hotmail.com>
2016-10-31 11:18:07 +0700

    Before SVM

--- dic_idf.py
+++ dic_idf.py
@@ -375,20 +375,20 @@ for seed in range(1,11):
 
         i+=1
     '''
-    '''
-    test_x_panda = pd.DataFrame(test_x).to_csv('3mood_add/seed'+str(seed)+'/'+'seed'+str(seed)+'_test_x.csv')
+
+    test_x_panda = pd.DataFrame(test_x).to_csv('test_x.csv')
     #test_x = pd.read_csv(str(seed)+'mood/seed'+str(seed)+'/'+'seed'+str(seed)+'_test_x.csv',sep=',').values[:,1:].tolist()
     #print(test_x)
-    train_x_panda = pd.DataFrame(training_x).to_csv('3mood_add/seed'+str(seed)+'/'+'seed'+str(seed)+'_train_x.csv')
+    train_x_panda = pd.DataFrame(training_x).to_csv('train_x.csv')
     #training_x = pd.read_csv(str(seed)+'mood/seed'+str(seed)+'/'+'seed'+str(seed)+'_train_x.csv',sep=',').values[:,1:].tolist()
     #print(training_x)
-    test_y_panda = pd.DataFrame(test_y).to_csv('3mood_add/seed'+str(seed)+'/'+'seed'+str(seed)+'_test_y.csv')
+    test_y_panda = pd.DataFrame(test_y).to_csv('test_y.csv')
     #test_y = list(np.reshape(pd.read_csv(str(seed)+'mood/seed'+str(seed)+'/'+'seed'+str(seed)+'_test_y.csv',sep=',').values[:,1:].tolist(),-1))
     #print(test_y)
-    train_y_panda = pd.DataFrame(training_y).to_csv('3mood_add/seed'+str(seed)+'/'+'seed'+str(seed)+'_train_y.csv')
+    train_y_panda = pd.DataFrame(training_y).to_csv('train_y.csv')
     #training_y = list(np.reshape(pd.read_csv(str(seed)+'mood/seed'+str(seed)+'/'+'seed'+str(seed)+'_train_y.csv',sep=',').values[:,1:].tolist(),-1))
     #print(training_y)
-    '''
+
     #///
     '''
     max_train_index = num_class_train.index(max(num_class_train))
@@ -411,6 +411,7 @@ for seed in range(1,11):
             training_x.append(list(tmp[j%len(idx)]))
             training_y.append(i)
     print(training_x)
+    '''
     '''
     pd_traning_x = pd.DataFrame(training_x)
     pd_test_x = pd.DataFrame(test_x)
@@ -518,7 +519,7 @@ for seed in range(1,11):
             for j in range(CLASS_NUM):
                 f.write(str(matrix[i,j]) + ' ')
             f.write(']\n')
-
+'''
 
 '''
 '''
